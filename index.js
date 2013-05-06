757680db270081cb170e3333423a586df142cdf8
const314 <konstantin.panchenko@devexpress.com>
2013-05-06 12:23:51 +0400

    PhoneJS Application Template

---
+++ index.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+
+window.MyApp = {};
+
+$(function() {
+    MyApp.app = new DevExpress.framework.html.HtmlApplication({
+        namespace: MyApp,
+        
+        defaultLayout: "navbar",
+        navigation: [
+          {
+            title: "Home",
+            action: "#home",
+            icon: "home"
+          },
+          {
+            title: "About",
+            action: "#about",
+            icon: "info"
+          }
+        ]
+    });
+    MyApp.app.router.register(":view", {view: "home"});
+    MyApp.app.navigate();   
+});
